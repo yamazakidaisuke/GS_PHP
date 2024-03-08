97a8955dd8340a45b12dc02508f5e44a63de2641
Daisu_yamazaki <yamazaki_daisuke@hotmail.co.jp>
2024-03-08 15:03:19 +0900

    Add files via upload

--- php/php04/menu.php
+++ php/php04/menu.php
@@ -3,7 +3,6 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="select.php">アンケート一覧</a>　
             <a class="navbar-brand" href="user.php">ユーザー登録</a>　
-            <a class="navbar-brand" href="user_select.php">ユーザー一覧</a>　
             <a class="navbar-brand" href="logout.php">ログアウト</a>
         </div>
     </div>
