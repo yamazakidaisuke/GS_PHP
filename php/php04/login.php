abfe968a81c0e4e3efa6ce6f2f07f1ed068d4bba
Daisu_yamazaki <yamazaki_daisuke@hotmail.co.jp>
2024-03-09 10:05:54 +0900

    Add files via upload

--- php/php04/login.php
+++ php/php04/login.php
@@ -16,9 +16,9 @@
 
 <!-- lLOGINogin_act.php は認証処理用のPHPです。 -->
 <form name="form1" action="login_act.php" method="post">
-ID:<input type="text" name="lid" />
-PW:<input type="password" name="lpw" />
-<input type="submit" value="LOGIN" />
+ID:<input type="text" name="lid">
+PW:<input type="password" name="lpw">
+<input type="submit" value="ログイン">
 </form>
 
 
